a2246531423cd52e6d9de0e0d06141f85b966986
Luis Guilherme <luislebert01@outlook.com>
2023-03-07 10:15:17 -0300

    Removido redundancias

--- JuntarVetores/Program.cs
+++ JuntarVetores/Program.cs
@@ -1,43 +1,41 @@
 ﻿// Primeiramente os vetores devem ser declarados
-using System.Drawing;
 
 int[] A, B, C;
 
-// Construindo e determinando espaço dos vetores
-A = new int[5];
-B = new int[5];
-C = new int[A.Length + B.Length];
-
 // Populando vetores A e B
 Console.WriteLine("Vetor A:");
-A = PopularVetor(A);
+A = PopularVetor(new int[5]);
 Console.WriteLine();
 Console.WriteLine("Vetor B:");
-B = PopularVetor(B);
+B = PopularVetor(new int[5]);
 
 int[] PopularVetor(int[] vet)
 {
     for (int i = 0; i < vet.Length; i++)
     {
-        Console.WriteLine("Posição {0} do vetor:", i + 1);
+        Console.WriteLine("Posição [{0}] do vetor:", i + 1);
         vet[i] = int.Parse(Console.ReadLine());
     }
     return vet;
 }
 
 // Juntando vetores:
-C = JuntarVetores(A, B, C);
+C = JuntarVetores(A, B);
 
-int[] JuntarVetores(int[] vetA, int[] vetB, int[] vetFinal)
+int[] JuntarVetores(int[] vetA, int[] vetB)
 {
+    int[] vetFinal = new int[vetA.Length + vetB.Length];
+
     for (int i = 0; i < vetA.Length; i++)
     {
         vetFinal[i] = vetA[i];
     }
 
+    int tamA = vetA.Length;
+
     for (int i = 0; i < vetB.Length; i++)
     {
-        vetFinal[i + vetA.Length] = vetB[i];
+        vetFinal[i + tamA] = vetB[i];
     }
 
     return vetFinal;
